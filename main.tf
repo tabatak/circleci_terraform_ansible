5e938a9d9dc4461b1871c94e6ccd80f50170b6ec
softhamamoto <kazuto.sc.hamamoto@jrits.ricoh.co.jp>
2017-03-31 14:05:09 +0900

    first

---
+++ main.tf
@@ -0,0 +1,108 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "ap-northeast-1"
+}
+variable "key_pair_name" {}
+variable "pvivate_key_path" {}
+
+
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "${var.region}"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route" "route_to_igw" {
+    route_table_id = "${aws_vpc.main.main_route_table_id}"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+}
+
+resource "aws_route_table_association" "a" {
+    subnet_id = "${aws_subnet.main.id}"
+    route_table_id = "${aws_vpc.main.main_route_table_id}"
+}
+
+resource "aws_security_group" "allow_ssh_httpd" {
+    name = "allow_ssh_httpd"
+    description = "Allow ssh and HTTP inbound traffic"
+    vpc_id = "${aws_vpc.main.id}"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "web" {
+    depends_on = ["aws_internet_gateway.igw"]
+    ami = "ami-56d4ad31"
+    key_name = "${var.key_pair_name}"
+    subnet_id = "${aws_subnet.main.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_ssh_httpd.id}"]
+    instance_type = "t2.micro"
+    tags {
+        Name = "Terraform"
+    }
+}
+
+resource "aws_eip" "web" {
+    depends_on = ["aws_internet_gateway.igw"]
+    instance = "${aws_instance.web.id}"
+    vpc = true
+
+    provisioner "file" {
+        source = "web/index.html"
+        destination = "/tmp/index.html"
+        connection {
+            host = "${self.public_ip}"
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "${file(var.pvivate_key_path)}"
+        }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum install -y httpd",
+            "sudo cp /tmp/index.html /var/www/html",
+            "sudo service httpd start",
+            "sleep 10"
+        ]
+        connection {
+            host = "${self.public_ip}"
+            type = "ssh"
+            user = "ec2-user"
+            private_key = "${file(var.pvivate_key_path)}"
+        }
+    }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+access_key="<access_key>"
+secret_key="<secret_key>"
+key_pair_name="terraform"
+pvivate_key_path="<terraform.pemのPATH(区切りは'/'で指定)>"
